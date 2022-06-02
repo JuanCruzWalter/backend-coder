04dd68f53a3c29b0eb1bf634bcf63832f36ec2c9
JuanCruzWalter <juancruzwalter@gmail.com>
2022-06-01 23:48:50 -0300

    agregando post

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,28 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const Contenedor = require('../contenedor')
+const productos = new Contenedor('products.txt')
+
+productos.read();
 
 const router = Router();
 
 router.get('/', (req, res) =>{
-    res.send({ message: 'Router of Products'})
+    res.send(productos.data)
 })
+
+router.get('/:id', async (req, res) =>{
+    const { id } = req.params;
+    const products = await productos.getByID(Number(id));
+    res.send(products)
+})
+
+router.post('/', (req, res)=>{
+    const { title, price, img } = req.body;
+    productos.save({title, price, img});
+    
+
+})
+
+
 
 module.exports = router;
